8426dbe6f869085961bc1d65f9b41f2036f24e3b
Demis Bellot <demis.bellot@gmail.com>
2015-09-15 01:43:16 -0700

    OSX is case-sensitive!

--- src/RedisReact/RedisReact.AppWinForms/FormMain.Designer.cs
+++ src/RedisReact/RedisReact.AppWinForms/FormMain.Designer.cs
@@ -33,10 +33,12 @@
             // 
             // FormMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(426, 403);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "FormMain";
             this.Text = "FormMain";
             this.ResumeLayout(false);
